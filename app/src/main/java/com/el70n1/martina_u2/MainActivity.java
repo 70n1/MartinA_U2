8c15fe4caf2a8a8c03d23816c12ef4f2991964b2
70n1 <el.70n1@gmail.com>
2016-09-26 21:47:33 +0200

    pestaña calculadora

--- app/src/main/java/com/el70n1/martina_u2/MainActivity.java
+++ app/src/main/java/com/el70n1/martina_u2/MainActivity.java
@@ -5,10 +5,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
     private FragmentTabHost tabHost;
+
+    private double operando_1 = 0;
+    private double operando_2 = 0;
+    private double ratio_euro = 166.39;
+    private boolean realizando_operacion = false;
+    private boolean en_euros = true;
+    private boolean reseteando = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +31,124 @@ public class MainActivity extends FragmentActivity {
 
         tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Botón"),
                 Tab2.class, null);
-        /*
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Lengüeta 3"),
+
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Calculadora"),
                 Tab3.class, null);
-                */
+
     }
+    public void sePulsa(View view){
+        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void pulsado_boton_calculadora(View view) {
+        String resourceEntryName = getResources().getResourceEntryName(view.getId());
+
+
+        if ((reseteando) && (!(resourceEntryName.equals("button_pasar_moneda")))) {
+            operando_1 = 0;
+            operando_2 = 0;
+            realizando_operacion = false;
+            reseteando= false;
+        }
+        if (resourceEntryName.equals("button_calc_0")) {
+            anyadir_numero_calculadora(0);
+        }
+        if (resourceEntryName.equals("button_calc_1")) {
+            anyadir_numero_calculadora(1);
+        }
+        if (resourceEntryName.equals("button_calc_2")) {
+            anyadir_numero_calculadora(2);
+        }
+        if (resourceEntryName.equals("button_calc_3")) {
+            anyadir_numero_calculadora(3);
+        }
+        if (resourceEntryName.equals("button_calc_4")) {
+            anyadir_numero_calculadora(4);
+        }
+        if (resourceEntryName.equals("button_calc_5")) {
+            anyadir_numero_calculadora(5);
+        }
+        if (resourceEntryName.equals("button_calc_6")) {
+            anyadir_numero_calculadora(6);
+        }
+        if (resourceEntryName.equals("button_calc_7")) {
+            anyadir_numero_calculadora(7);
+        }
+        if (resourceEntryName.equals("button_calc_8")) {
+            anyadir_numero_calculadora(8);
+        }
+        if (resourceEntryName.equals("button_calc_9")) {
+            anyadir_numero_calculadora(9);
+        }
+
+        if (resourceEntryName.equals("button_calc_mas")) {
+            operando_1 = operando_1 + operando_2;
+            operando_2 = 0;
+            realizando_operacion = true;
+        }
+
+        if (resourceEntryName.equals("button_calc_igual")) {
+            operando_1 = operando_1 + operando_2;
+            operando_2 = 0;
+            realizando_operacion = false;
+        }
+        if (resourceEntryName.equals("button_calc_c")) {
+            operando_1 = 0;
+            operando_2 = 0;
+            realizando_operacion = false;
+        }
+        if (resourceEntryName.equals("button_pasar_moneda")){
+            if (en_euros) {
+                operando_1 = operando_1 * ratio_euro;
+                operando_2 = operando_2 * ratio_euro;
+                ((Button) findViewById(R.id.button_pasar_moneda)).setText("pasar a Euros");
+            } else {
+                if (operando_1>0) {
+                    operando_1 = operando_1 / ratio_euro;
+                } else {
+                    operando_1 = 0;
+                }
+                if (operando_2>0) {
+                    operando_2 = operando_2 / ratio_euro;
+                } else {
+                    operando_2 = 0;
+                }
+                ((Button) findViewById(R.id.button_pasar_moneda)).setText("pasar a pesetas");
+            }
+            en_euros = !(en_euros);
+            reseteando = true;
+        }
+        mostrar_display();
+    }
+
+    private void mostrar_display() {
+        String numero_a_mostrar="";
+        if (realizando_operacion) {
+            numero_a_mostrar = prettyPrint(operando_2);
+        } else{
+            numero_a_mostrar = prettyPrint(operando_1);
+        }
+        if (en_euros) {
+            ((TextView) findViewById(R.id.display_calculadora)).setText(numero_a_mostrar +" €");
+        } else {
+            ((TextView) findViewById(R.id.display_calculadora)).setText(numero_a_mostrar +" ptas");
+        }
+    }
+
+    public static String prettyPrint(double d) {
+        int i = (int) d;
+        return d == i ? String.valueOf(i) : String.format("%.2f", d);
+    }
+
+    private void anyadir_numero_calculadora(int numero){
+        if (realizando_operacion) {
+            operando_2 = (operando_2 *10) + numero;
+        } else {
+            operando_1 = (operando_1 *10) + numero;
+        }
+    }
+
 }
 
 
--- app/src/main/java/com/el70n1/martina_u2/Tab3.java
+++ app/src/main/java/com/el70n1/martina_u2/Tab3.java
@@ -5,26 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 
 /**
- * Created by motoni on 23/09/2016.
+ * Created by motoni on 26/09/2016.
  */
 
-public class Tab2 extends Fragment {
+public class Tab3 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab2, container, false);
+        return inflater.inflate(R.layout.tab3, container, false);
     }
 
-    /*
-    public void sePulsa(View view){
-        Toast.makeText(getActivity(), "Pulsado", Toast.LENGTH_SHORT).show();
-    }
-    */
 }
